f803b2a72653e4bd59f865987846e6dd99f9c9cf
Tii <mail@tii.one>
2024-10-18 11:22:10 +0000

    Added Sail for DevContainers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        if (env('APP_ENV') === 'local') {
+            $middleware->trustProxies(at: '*');
+        }
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
